ee416ac0e41cc2a4d29fc8f51c84b088f33f8aa5
Steun <teunkelting7@gmail.com>
2015-09-01 16:09:23 +0200

    bob je moeder

--- server.js
+++
@@ -1,7 +0,0 @@
-var express     = require('express'),
-    config      = require('./config/config'),
-    glob        = require('glob');
-
-var app = express();
-require('./config/express')(app, config);
-app.listen(config.port);
