4666a7dfd93da52110d88f17d3f16de46ee3e8c3
Bob Steen <13983@ma-web.nl>
2015-09-04 20:23:50 +0200

    added smooth scrolling

---
+++ public/dist/scripts/main.js
@@ -0,0 +1,20 @@
+$("#click-team").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#team").offset().top
+    }, 1000);
+});
+$("#click-cover").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#cover").offset().top
+    }, 1000);
+});
+$("#click-contact").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 1000);
+});
+$("#click-down").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#team").offset().top
+    }, 1000);
+});
